03de22002508c3242608bef393a073b1369575ed
alex0109 <krossyouyub@gmail.com>
2021-05-01 01:06:14 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Task from './components/Task';
+import TaskInput from './components/TaskInput';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      tasks: [
+        {id: 0, title: 'First Task', done: false },
+        {id: 1, title: 'Second Task', done: true },
+        {id: 2, title: 'Third Task', done: false }
+      ]
+    }
+  }
+
+  addTask = task => {
+    this.setState(state => {
+      let { tasks } = state;
+      tasks.push({
+        id: tasks.length !== 0 ? tasks.length : 0,
+        title: task,
+        done: false
+      });
+      return tasks;
+    });
+  }
+
+  doneTask = (id) => {
+    const index = this.state.tasks.map(task => task.id).indexOf(id);
+    this.setState(state => {
+      let { tasks } = state;
+      tasks[index].done = true;
+      return tasks;
+    })
+  }
+
+  deleteTask = (id) => {
+    const index = this.state.tasks.map(task => task.id).indexOf(id);
+    this.setState(state => {
+      let { tasks } = state;
+      delete tasks[index];
+      return tasks;
+    })
+  }
+
+  render() {
+
+    const { tasks } = this.state;
+    const activeTasks = tasks.filter(task => !task.done);
+    const doneTasks = tasks.filter(task => task.done);
+
+    return(
+      <div className="app">
+        <h1 className="top">Active tasks: {activeTasks.length}</h1>
+        {[...activeTasks, ...doneTasks].map(task => (
+          <Task doneTask={() => this.doneTask(task.id)} deleteTask={() => this.deleteTask(task.id)} task={task} key={task.id}/>
+        ))}
+        <TaskInput addTask={this.addTask}/>
+      </div>
+    );
+  }
+
 }
 
 export default App;
